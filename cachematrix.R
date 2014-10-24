0941430be9181d67007c81da07ea4737cd2ddbfa
Lotfi <lotfelee@gmail.com>
2014-10-24 22:48:13 +0400

    R programming third week assignment with comments in in the code

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,9 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Below are two functions that are used to create a special object that stores a matrix and cache's its inverse.
 
-## Write a short comment describing this function
+
+
+## This function take a matrix as argument and return a spical matrix which is a list of function ,getInv to read the inverse of th
+##of the matrix and and setInv to set it ,get to read the matrix ,and set to assign value to it.
 
 makeCacheMatrix <- function(x = matrix()) {
         Inv <- NULL
@@ -18,7 +20,7 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## The following function calculates the inverse of the special "matrix" created with the above function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
